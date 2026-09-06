29d744cb5e15ddcef956bb1dbd81976808eed2a0
Mariem Rmili <rmilimariem013@gmail.com>
2026-09-06 17:02:14 +0100

    Add privilege escalation and file tampering test scripts

---
+++ tests/file_tampering_test.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+# Attack C: Sensitive File Tampering Simulation
+
+echo "[+] Simulating sensitive file access & tampering..."
+sudo cat /etc/sudoers
+sudo cat /etc/passwd | tail -5
+sudo touch /etc/passwd
+
+
+# chmod +x tests/file_tampering_test.sh
---
+++ tests/privilege_escalation_test.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+# Attack A: Privilege Escalation Reconnaissance Simulation
+
+echo "[+] Simulating privilege escalation recon..."
+whoami
+id
+uname -a
+sudo -l
+cat /etc/passwd
+sudo cat /etc/shadow
+find / -perm -4000 -type f 2>/dev/null
+
+#chmod +x tests/privilege_escalation_test.sh
